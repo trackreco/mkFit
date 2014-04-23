4bda7d380cbe062bf0ac43ce6b79ac6db64a67c8
Matevz Tadel <mtadel@ucsd.edu>
2014-04-23 14:58:56 -0700

    Fix a bug. Add cramer inversion for 2x2 and 3x3 matrices.

--- Matriplex.h
+++ Matriplex.h
@@ -1,6 +1,8 @@
 #ifndef Matriplex_H
 #define Matriplex_H
 
+#include <cstdio>
+
 typedef int idx_t;
 
 template<typename T, idx_t D1, idx_t D2, idx_t N>
@@ -37,9 +39,9 @@ public:
 
    void Assign(idx_t n, T *arr)
    {
-      for (idx_t i = n; n < kTotSize; n += N)
+      for (idx_t i = n; i < kTotSize; i += N)
       {
-         fArray[i] = *arr++;
+         fArray[i] = *(arr++);
       }
    }
 };
@@ -48,23 +50,25 @@ public:
 template<typename T, idx_t D1, idx_t D2, idx_t D3, idx_t N>
 void Multiply(const Matriplex<T, D1, D2, N>& A,
               const Matriplex<T, D2, D3, N>& B,
-              Matriplex<T, D1, D3, N>& C)
+                    Matriplex<T, D1, D3, N>& C)
 {
    for (idx_t i = 0; i < D1; ++i)
    {
-      const idx_t io = i * N * D2;
       for (idx_t j = 0; j < D3; ++j)
       {
-         const idx_t ijo = io + j * N;
+         const idx_t ijo = N * (i * D3 + j);
+
+         for (idx_t n = 0; n < N; ++n)
+         {
+            C.fArray[ijo + n] = 0;
+         }
+
          for (idx_t k = 0; k < D2; ++k)
          {
-            for (idx_t n = 0; n < N; ++n)
-            {
-               C.fArray[ijo + n] = 0;
-            }
+            const idx_t iko = N * (i * D2 + k);
+            const idx_t kjo = N * (k * D3 + j);
 
-            const idx_t iko = io + k * N;
-            const idx_t kjo = N * (k * D2 + j);
+#pragma simd
             for (idx_t n = 0; n < N; ++n)
             {
             // C.fArray[i, j, n] += A.fArray[i, k, n] * B.fArray[k, j, n];
@@ -74,5 +78,109 @@ void Multiply(const Matriplex<T, D1, D2, N>& A,
       }
    }
 }
+
+
+//==============================================================================
+
+
+template<typename T, idx_t D, idx_t N>
+struct CramerInverter
+{
+   void operator()(Matriplex<T, D, D, N>& C, double *determ=0)
+   {
+      // We don't do general Inverts.
+
+      if (determ)
+      {
+         for (idx_t n = 0; n < N; ++n)
+         {
+            determ[n] = 0;
+         }
+      }
+   }
+};
+
+
+template<typename T, idx_t N>
+struct CramerInverter<T, 2, N>
+{
+   void operator()(Matriplex<T, 2, 2, N>& C, double *determ=0)
+   {
+      typedef T TT;
+
+#pragma simd
+      for (idx_t n = 0; n < N; ++n)
+      {
+         T *pM = & C.fArray[0];
+
+         const TT det = pM[n] * pM[3*N + n] - pM[2*N + n] * pM[N + n];
+
+         //if (determ)
+         //determ[n] = s;
+
+         //if (det == 0)
+         {
+            const TT s   = TT(1) / det;
+            const TT tmp = s * pM[3*N + n];
+            pM[N + n]   *= -s;
+            pM[2*N + n] *= -s;
+            pM[3*N + n]  = s * pM[n];
+            pM[n] = tmp;
+         }
+      }
+   }
+};
+
+template<typename T, idx_t N>
+struct CramerInverter<T, 3, N>
+{
+   void operator()(Matriplex<T, 3, 3, N>& C, double *determ=0)
+   {
+      typedef T TT;
+
+#pragma simd
+      for (idx_t n = 0; n < N; ++n)
+      {
+         T *pM = & C.fArray[n];
+
+         const TT c00 = pM[4*N] * pM[8*N] - pM[5*N] * pM[7*N];
+         const TT c01 = pM[5*N] * pM[6*N] - pM[3*N] * pM[8*N];
+         const TT c02 = pM[3*N] * pM[7*N] - pM[4*N] * pM[6*N];
+         const TT c10 = pM[7*N] * pM[2*N] - pM[8*N] * pM[1*N];
+         const TT c11 = pM[8*N] * pM[0*N] - pM[6*N] * pM[2*N];
+         const TT c12 = pM[6*N] * pM[1*N] - pM[7*N] * pM[0*N];
+         const TT c20 = pM[1*N] * pM[5*N] - pM[2*N] * pM[4*N];
+         const TT c21 = pM[2*N] * pM[3*N] - pM[0*N] * pM[5*N];
+         const TT c22 = pM[0*N] * pM[4*N] - pM[1*N] * pM[3*N];
+
+         const TT det = pM[0*N] * c00 + pM[1*N] * c01 + pM[2*N] * c02;
+
+         //if (determ)
+         //  *determ[n] = det;
+
+         const TT s = TT(1) / det;
+
+         pM[0*N] = s*c00;
+         pM[1*N] = s*c10;
+         pM[2*N] = s*c20;
+         pM[3*N] = s*c01;
+         pM[4*N] = s*c11;
+         pM[5*N] = s*c21;
+         pM[6*N] = s*c02;
+         pM[7*N] = s*c12;
+         pM[8*N] = s*c22;
+      }
+   }
+};
+
+template<typename T, idx_t D, idx_t N>
+void InvertCramer(Matriplex<T, D, D, N>& C, double *determ=0)
+{
+   // We don't do general Inverts.
+
+   CramerInverter<T, D, N> ci;
+   ci(C, determ);
+}
+
 
 #endif
