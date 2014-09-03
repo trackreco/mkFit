fd21629985bf83bc3ec0fb4f7c46044ac23b72af
Peter Wittich <wittich@cornell.edu>
2014-09-03 16:33:32 +0200

    octagonal geometry

--- fittest.cc
+++ fittest.cc
@@ -101,6 +101,23 @@ void runFittingTest(bool saveTree, unsigned int Ntracks, Geometry* theGeom)
     Track simtrk(q,pos,mom,covtrk,hits,0.,initHits);
     simtracks.push_back(simtrk);
   }
+#define DUMPPW
+#ifdef DUMPPW
+  for (unsigned int itrack=0;itrack<simtracks.size();++itrack) {
+    Track& trk = simtracks[itrack];
+
+    std::vector<Hit>& initHits = trk.initHitsVector();
+    for ( auto i = initHits.begin(); i != initHits.end(); ++i ) {
+      std::cout << "PWHIT: "
+		<< i->position()[0] << " "
+		<< i->position()[1]<< " "
+		<< i->position()[2]<< " "
+		<< std::endl;
+    }
+
+  }
+  
+#endif // DUMPPW
 
   for (unsigned int itrack=0;itrack<simtracks.size();++itrack) {
     bool dump = false;
--- main.cc
+++ main.cc
@@ -10,6 +10,8 @@
 #include "buildtest.h"
 #include "Geometry.h"
 #include "USolids/include/UTubs.hh"
+#include "USolids/include/UPolyHedra.hh"
+#include "USolids/include/USphere.hh"
 
 int main(){
   Geometry* theGeom = new Geometry;
@@ -17,16 +19,26 @@ int main(){
   // NB: we currently assume that each node is a layer, and that layers
   // are added starting from the center
   for (int l = 0; l < 10; l++) {
-    float r = (l+1)*4;
+    float r = (l+1)*4.;
+    //#define CYLINDER
+#ifdef CYLINDER
     std::string s = "Cylinder" + std::string(1, 48+l);
     UTubs* utub = new UTubs(s, r, r+.01, 100.0, 0, TMath::TwoPi());
     theGeom->AddLayer(utub);
+#else
+    std::string s = "PolyHedra" + std::string(1, 48+l);
+    const double zPlane[] = {-100.,100.};
+    const double rInner[] = {r,r};
+    const double rOuter[] = {r+.01,r+.01};
+    UPolyhedra* upolyh = new UPolyhedra(s, 0, TMath::TwoPi(), 8, 2, zPlane, rInner, rOuter);
+    theGeom->AddLayer(upolyh);
+#endif
   }
 
   bool saveTree = true;
   runFittingTest(saveTree,50000,theGeom); 
   //runFittingTestPlex(saveTree,theGeom); 
-  runBuildingTest(saveTree,100,theGeom); 
+  //runBuildingTest(saveTree,100,theGeom); 
 
   delete theGeom;
   return 0;
