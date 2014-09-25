45b17cb9a435c3274686f02c69d607f62b488f01
Matevz Tadel <mtadel@ucsd.edu>
2014-09-25 13:11:16 -0700

    Changes from mtorture.

--- Matrix.cc
+++ Matrix.cc
@@ -57,3 +57,5 @@ void dumpMatrix(SMatrixSym66& m){
 std::default_random_engine            g_gen(0xbeef0133);
 std::normal_distribution<float>       g_gaus(0.0, 1.0);
 std::uniform_real_distribution<float> g_unif(0.0, 1.0);
+
+bool g_dump = false;
--- Matrix.h
+++ Matrix.h
@@ -1,6 +1,10 @@
 #ifndef _matrix_
 #define _matrix_
 
+#ifndef MAX_HITS
+#define MAX_HITS 10
+#endif
+
 #include "Math/SMatrix.h"
 
 typedef ROOT::Math::SMatrix<float,6,6,ROOT::Math::MatRepSym<float,6> >    SMatrixSym66;
@@ -23,13 +27,76 @@ void dumpMatrix(SMatrixSym66& m);
 
 //==============================================================================
 
+// This should go elsewhere, eventually.
+
+#include <sys/time.h>
+
+inline double dtime()
+{
+    double tseconds = 0.0;
+    struct timeval mytime;
+    gettimeofday(&mytime,(struct timezone*)0);
+    tseconds = (double)(mytime.tv_sec + mytime.tv_usec*1.0e-6);
+    return( tseconds );
+}
+
+//==============================================================================
+
+// This ifdef needs to be changed to something like "use matriplex" and/or
+// "is icc" as we can only do vectorization with icc now.
+
+#ifndef __APPLE__
+
+#include "Matriplex/MatriplexSym.h"
+
+#ifndef MPT_SIZE
+#define MPT_SIZE 16
+#endif
+
+typedef int idx_t;
+
+const idx_t NN =  MPT_SIZE; // "Length" of MPlex.
+
+const idx_t LL =  6; // Dimension of large/long  MPlex entities
+const idx_t HH =  3; // Dimension of small/short MPlex entities
+
+typedef Matriplex::Matriplex<float, LL, LL, NN>   MPlexLL;
+typedef Matriplex::Matriplex<float, LL,  1, NN>   MPlexLV;
+typedef Matriplex::MatriplexSym<float, LL,  NN>   MPlexLS;
+
+typedef Matriplex::Matriplex<float, HH, HH, NN>   MPlexHH;
+typedef Matriplex::Matriplex<float, HH,  1, NN>   MPlexHV;
+typedef Matriplex::MatriplexSym<float, HH,  NN>   MPlexHS;
+
+typedef Matriplex::Matriplex<float, LL, HH, NN>   MPlexLH;
+
+typedef Matriplex::Matriplex<float, 1, 1, NN>     MPlexQF;
+typedef Matriplex::Matriplex<int,   1, 1, NN>     MPlexQI;
+
+#endif
+
+#ifndef NUM_THREADS
+#define NUM_THREADS 1
+#endif
+
+#ifndef THREAD_BINDING
+#define THREAD_BINDING spread
+#endif
+
+//==============================================================================
+
 #include <random>
 
 extern std::default_random_engine            g_gen;
 extern std::normal_distribution<float>       g_gaus;
 extern std::uniform_real_distribution<float> g_unif;
 
-#ifndef NO_ROOT
+// All debug printouts are ifdefed with DEBUG
+// #define DEBUG
+
+extern bool g_dump;
+
+#ifdef NO_ROOT
 
 typedef double Double_t;
 
