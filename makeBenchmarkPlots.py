26628711f9d30b3931896b90f70e2c68c91362b2
Giuseppe <giuseppe.cerati@cern.ch>
2015-12-18 15:23:50 +0100

    more plotting utilities

--- makeBenchmarkPlots.py
+++ makeBenchmarkPlots.py
@@ -2,64 +2,103 @@ import os.path, glob, sys
 import ROOT
 import array
 
-g = ROOT.TFile("benchmark.root","recreate")
+hORm = 'host'
+#hORm = 'mic'
 
-for test in {'BH','CE','CEST'}:
-    pos = 15
-    if 'BH' in test: pos = 9
-    g_HOST_VU = ROOT.TGraph(4)
-    g_HOST_VU_speedup = ROOT.TGraph(4)
+g = ROOT.TFile('benchmark_'+hORm+'.root',"recreate")
+
+for test in ['BH','CE','CEST','FIT']:
+    print test
+    pos = 14
+    if 'BH' in test: pos = 8
+    if 'FIT' in test: pos = 3
+    g_VU = ROOT.TGraph(4)
+    g_VU_speedup = ROOT.TGraph(4)
     point = 0
     for vu in [1,2,4,8]:
-        os.system('grep Total log_10x20k_'+test+'_NVU'+str(vu)+'_NTH1.txt >& log_10x20k_'+test+'_VU.txt')
-        with open('log_10x20k_'+test+'_VU.txt') as f:
+        os.system('grep Matriplex log_'+hORm+'_10x20k_'+test+'_NVU'+str(vu)+'_NTH1.txt >& log_'+hORm+'_10x20k_'+test+'_VU.txt')
+        xval = float(vu)
+        yval = 0.
+        firstFound = False
+        with open('log_'+hORm+'_10x20k_'+test+'_VU.txt') as f:
             for line in f:
+                if 'Matriplex' not in line: continue
+                if 'Total' in line: continue
                 lsplit = line.split()
-                xval = float(vu)
-                yval = float(lsplit[pos])
-                print xval, yval
-                g_HOST_VU.SetPoint(point,xval,yval)
-                point = point+1
+                if 'FIT' in test: 
+                    tmp = float(lsplit[pos])
+                    if firstFound is False or tmp<yval: yval=tmp
+                    if not firstFound:
+                        firstFound = True
+                else:
+                    if not firstFound:
+                        firstFound = True
+                        continue
+                    yval = yval+float(lsplit[pos])
+        if 'FIT' not in test: yval = 10.*yval/9.
+        print xval, yval
+        g_VU.SetPoint(point,xval,yval)
+        point = point+1
     x0 = array.array('d',[0])
     y0 = array.array('d',[0])
-    g_HOST_VU.GetPoint(0,x0,y0)
+    g_VU.GetPoint(0,x0,y0)
     point = 0
     for vu in [1,2,4,8]:
         xval = array.array('d',[0])
         yval = array.array('d',[0])
-        g_HOST_VU.GetPoint(point,xval,yval)
-        g_HOST_VU_speedup.SetPoint(point,xval[0],y0[0]/yval[0])
+        g_VU.GetPoint(point,xval,yval)
+        speedup = 0.
+        if yval[0]>0.: speedup = y0[0]/yval[0]
+        g_VU_speedup.SetPoint(point,xval[0],speedup)
         point = point+1
-    g_HOST_VU.Write("g_HOST_"+test+"_VU")
-    g_HOST_VU_speedup.Write("g_HOST_"+test+"_VU_speedup")
+    g_VU.Write("g_"+test+"_VU")
+    g_VU_speedup.Write("g_"+test+"_VU_speedup")
 
-    g_HOSTTH = ROOT.TGraph(4)
-    g_HOSTTH_speedup = ROOT.TGraph(4)
     point = 0
-    for th in [1,3,7,21]:
-        os.system('grep Total log_10x20k_'+test+'_NVU8_NTH'+str(th)+'.txt >& log_10x20k_'+test+'_TH.txt')
-        with open('log_10x20k_'+test+'_TH.txt') as f:
+    nvu = '8'
+    if hORm == 'mic': nvu = '16'
+    thvals = [1,3,7,21]
+    if hORm == 'mic' and 'FIT' in test: thvals = [1,3,7,21,42,63,84,105,126,147,168,189,210]
+    g_TH = ROOT.TGraph(len(thvals))
+    g_TH_speedup = ROOT.TGraph(len(thvals))
+    for th in thvals:
+        os.system('grep Matriplex log_'+hORm+'_10x20k_'+test+'_NVU'+nvu+'_NTH'+str(th)+'.txt >& log_'+hORm+'_10x20k_'+test+'_TH.txt')
+        xval = float(th)
+        yval = 0.
+        firstFound = False
+        with open('log_'+hORm+'_10x20k_'+test+'_TH.txt') as f:
             for line in f:
+                if 'Matriplex' not in line: continue
+                if 'Total' in line: continue
                 lsplit = line.split()
-                xval = float(th)
-                yval = float(lsplit[pos])
-                print xval, yval
-                g_HOSTTH.SetPoint(point,xval,yval)
-                point = point+1
+                if 'FIT' in test: 
+                    tmp = float(lsplit[pos])
+                    if firstFound is False or tmp<yval: yval=tmp
+                    if not firstFound:
+                        firstFound = True
+                else:
+                    if not firstFound:
+                        firstFound = True
+                        continue
+                    yval = yval+float(lsplit[pos])
+        if 'FIT' not in test: yval = 10.*yval/9.
+        print xval, yval
+        g_TH.SetPoint(point,xval,yval)
+        point = point+1
     x0 = array.array('d',[0])
     y0 = array.array('d',[0])
-    g_HOSTTH.GetPoint(0,x0,y0)
+    g_TH.GetPoint(0,x0,y0)
     point = 0
-    for th in [1,3,7,21]:
+    for th in thvals:
         xval = array.array('d',[0])
         yval = array.array('d',[0])
-        g_HOSTTH.GetPoint(point,xval,yval)
-        g_HOSTTH_speedup.SetPoint(point,xval[0],y0[0]/yval[0])
+        g_TH.GetPoint(point,xval,yval)
+        speedup = 0.
+        if yval[0]>0.: speedup = y0[0]/yval[0]
+        g_TH_speedup.SetPoint(point,xval[0],speedup)
         point = point+1
-    g_HOSTTH.Write("g_HOST_"+test+"_TH")
-    g_HOSTTH_speedup.Write("g_HOST_"+test+"_TH_speedup")
-
-
+    g_TH.Write("g_"+test+"_TH")
+    g_TH_speedup.Write("g_"+test+"_TH_speedup")
 
 g.Write()
 g.Close()
---
+++ makeBenchmarkPlotsFit.C
@@ -0,0 +1,98 @@
+{
+
+  bool isMic = false;
+  
+  TString hORm = "host";
+  if (isMic) hORm = "mic";
+
+  TFile* f = TFile::Open("benchmark_"+hORm+".root");
+
+  TCanvas c1;
+  TGraph* g_FIT_VU = (TGraph*) f->Get("g_FIT_VU");
+  g_FIT_VU->SetTitle("Vectorization benchmark on "+hORm);
+  g_FIT_VU->GetXaxis()->SetTitle("Vector Unit Utilization");
+  g_FIT_VU->GetYaxis()->SetTitle("Time for 20k tracks [s]");
+  g_FIT_VU->GetYaxis()->SetTitleOffset(1.25);
+  g_FIT_VU->GetXaxis()->SetRangeUser(1,8);
+  g_FIT_VU->GetYaxis()->SetRangeUser(0,0.2);
+  g_FIT_VU->SetLineWidth(2);
+  g_FIT_VU->SetLineColor(kBlue);
+  g_FIT_VU->SetMarkerStyle(kFullCircle);
+  g_FIT_VU->SetMarkerColor(kBlue);
+  g_FIT_VU->Draw("ALP");
+  TLegend* leg_VU = new TLegend(0.60,0.75,0.85,0.85);
+  leg_VU->SetBorderSize(0);
+  leg_VU->AddEntry(g_FIT_VU,"Fit","LP");
+  leg_VU->Draw();
+  c1.SetGridy();
+  c1.Update();
+  c1.SaveAs(hORm+"_vu_fittime.png");
+
+  TCanvas c2;
+  TGraph* g_FIT_VU_speedup = (TGraph*) f->Get("g_FIT_VU_speedup");
+  g_FIT_VU_speedup->SetTitle("Vectorization speedup on "+hORm);
+  g_FIT_VU_speedup->GetXaxis()->SetTitle("Vector Unit Utilization");
+  g_FIT_VU_speedup->GetYaxis()->SetTitle("Speedup");
+  g_FIT_VU_speedup->GetXaxis()->SetRangeUser(1,8);
+  g_FIT_VU_speedup->GetYaxis()->SetRangeUser(1,8);
+  // if (isMic) g_FIT_VU->GetXaxis()->SetRangeUser(1,16);
+  // if (isMic) g_FIT_VU->GetYaxis()->SetRangeUser(1,16);
+  g_FIT_VU_speedup->SetLineWidth(2);
+  g_FIT_VU_speedup->SetLineColor(kBlue);
+  g_FIT_VU_speedup->SetMarkerStyle(kFullCircle);
+  g_FIT_VU_speedup->SetMarkerColor(kBlue);
+  g_FIT_VU_speedup->Draw("ALP");
+  TLegend* leg_VU_speedup = new TLegend(0.20,0.75,0.45,0.85);
+  leg_VU_speedup->SetBorderSize(0);
+  leg_VU_speedup->AddEntry(g_FIT_VU_speedup,"Fit","LP");
+  leg_VU_speedup->Draw();
+  c2.SetGridy();
+  c2.Update();
+  c2.SaveAs(hORm+"_vu_fitspeedup.png");
+
+  TCanvas c3;
+  TGraph* g_FIT_TH = (TGraph*) f->Get("g_FIT_TH");
+  g_FIT_TH->SetTitle("Parallelization benchmark on "+hORm);
+  g_FIT_TH->GetXaxis()->SetTitle("Number of Threads");
+  g_FIT_TH->GetYaxis()->SetTitle("Time for 20k tracks [s]");
+  g_FIT_TH->GetYaxis()->SetTitleOffset(1.25);
+  g_FIT_TH->GetXaxis()->SetRangeUser(1,21);
+  g_FIT_TH->GetYaxis()->SetRangeUser(0,0.04);
+  if (isMic) g_FIT_TH->GetXaxis()->SetRangeUser(1,210);
+  if (isMic) g_FIT_TH->GetYaxis()->SetRangeUser(0,14);
+  g_FIT_TH->SetLineWidth(2);
+  g_FIT_TH->SetLineColor(kBlue);
+  g_FIT_TH->SetMarkerStyle(kFullCircle);
+  g_FIT_TH->SetMarkerColor(kBlue);
+  g_FIT_TH->Draw("ALP");
+  TLegend* leg_TH = new TLegend(0.60,0.75,0.85,0.85);
+  leg_TH->SetBorderSize(0);
+  leg_TH->AddEntry(g_FIT_TH,"Fit","LP");
+  leg_TH->Draw();
+  c3.SetGridy();
+  c3.Update();
+  c3.SaveAs(hORm+"_th_fittime.png");
+
+  TCanvas c4;
+  TGraph* g_FIT_TH_speedup = (TGraph*) f->Get("g_FIT_TH_speedup");
+  g_FIT_TH_speedup->SetTitle("Parallelization speedup on "+hORm);
+  g_FIT_TH_speedup->GetXaxis()->SetTitle("Number of Threads");
+  g_FIT_TH_speedup->GetYaxis()->SetTitle("Speedup");
+  g_FIT_TH_speedup->GetXaxis()->SetRangeUser(0,21);
+  g_FIT_TH_speedup->GetYaxis()->SetRangeUser(0,21);
+  if (isMic) g_FIT_TH_speedup->GetXaxis()->SetRangeUser(1,210);
+  if (isMic) g_FIT_TH_speedup->GetYaxis()->SetRangeUser(1,210);
+  g_FIT_TH_speedup->SetLineWidth(2);
+  g_FIT_TH_speedup->SetLineColor(kBlue);
+  g_FIT_TH_speedup->SetMarkerStyle(kFullCircle);
+  g_FIT_TH_speedup->SetMarkerColor(kBlue);
+  g_FIT_TH_speedup->Draw("ALP");
+  TLegend* leg_TH_speedup = new TLegend(0.20,0.75,0.45,0.85);
+  leg_TH_speedup->SetBorderSize(0);
+  leg_TH_speedup->AddEntry(g_FIT_TH_speedup,"Fit","LP");
+  leg_TH_speedup->Draw();
+  c4.SetGridy();
+  c4.Update();
+  c4.SaveAs(hORm+"_th_fitspeedup.png");
+
+}
