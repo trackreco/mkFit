25bd0b77f36ddb696e5ff5daa27cc8aca02f5b23
Matevz Tadel <mtadel@ucsd.edu>
2017-03-27 13:35:14 -0700

    Update the knl bencmarking notes.

--- runBenchmark-knl-notes.sh
+++ runBenchmark-knl-notes.sh
@@ -10,17 +10,10 @@ root -b -q -l makeBenchmarkPlotsFit.C\(0,0,1\)
 
 # Run parts of this where combined with mic/host.
 # Need to decide what we compare against ... or it will be a mess.
-for test in BH CE CEST ST TBBST; do
+for test in BH ST CE; do
 
-    # Orig
-    python makePlotsFromDump.py _host_10x20k_${test}_NVU1_NTH1
-    python makePlotsFromDump.py _host_10x20k_${test}_NVU8int_NTH21
-    python makePlotsFromDump.py _mic_10x20k_${test}_NVU1_NTH1
-    python makePlotsFromDump.py _mic_10x20k_${test}_NVU16int_NTH210
-
-    # KNL
-    python makePlotsFromDump.py _knl_10x20k_${test}_NVU1_NTH1
-    python makePlotsFromDump.py _knl_10x20k_${test}_NVU16int_NTH210
+    python makePlotsFromDump.py KNL ToyMC Barrel ${test} NVU1_NTH1
+    python makePlotsFromDump.py KNL ToyMC Barrel ${test} NVU16int_NTH210
 
     root -b -q -l makePlotsFromDump.C\(\"${test}\"\)
 done
