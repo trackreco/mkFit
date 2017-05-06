4abb8c0e31d73acc8b879f966a26e09ff1b0faf9
Slava Krutelyov <slava77@gmail.com>
2017-05-05 17:22:15 -0700

    add vv float

--- tkNtuple/DictsLinkDef.h
+++ tkNtuple/DictsLinkDef.h
@@ -3,9 +3,14 @@
 
 #ifdef __CINT__
 #pragma link C++ class vector<vector<int> >+ ;
+#pragma link C++ class vector<vector<float> >+ ;
 #ifdef G__VECTOR_HAS_CLASS_ITERATOR
 #pragma link C++ operators vector<vector<int> >::iterator;
 #pragma link C++ operators vector<vector<int> >::const_iterator;
 #pragma link C++ operators vector<vector<int> >::reverse_iterator;
+
+#pragma link C++ operators vector<vector<float> >::iterator;
+#pragma link C++ operators vector<vector<float> >::const_iterator;
+#pragma link C++ operators vector<vector<float> >::reverse_iterator;
 #endif
 #endif
