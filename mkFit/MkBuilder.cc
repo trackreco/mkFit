320f81bf37c4c9078bd2218ec88d39034558030a
Slava Krutelyov <slava77@cern.ch>
2017-08-04 13:09:09 -0700

    do not clean with label -1 if CLEAN_SEEDS is defined

--- mkFit/MkBuilder.cc
+++ mkFit/MkBuilder.cc
@@ -384,12 +384,14 @@ void MkBuilder::import_seeds()
 
   if (Config::readCmsswSeeds)
   {
+#ifndef CLEAN_SEEDS
     printf("***\n*** MkBuilder::import_seeds REMOVING SEEDS WITH BAD LABEL. This is a development hack. ***\n***\n");
 
     if (true) {
       TrackVec buf; m_event->seedTracks_.swap(buf);
       std::copy_if(buf.begin(), buf.end(), std::back_inserter(m_event->seedTracks_), [](const Track& t){ return t.label() >= 0; });
     }
+#endif
   }
 
   TrackerInfo &trk_info = Config::TrkInfo;
