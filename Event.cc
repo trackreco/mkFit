65863986043732f1e2ead13731e6d54c26dd35c4
Kevin McDermott <kpm82@cornell.edu>
2017-10-23 01:50:33 -0700

    rollback changes to data file header for reading sim trackstates

--- Event.cc
+++ Event.cc
@@ -469,19 +469,8 @@ void Event::read_in(DataFile &data_file, FILE *in_fp)
   {
     int nts; 
     fread(&nts, sizeof(int), 1, fp);
-    if (Config::readSimTrackStates)
-    {
-      simTrackStates_.resize(nts);
-      for (int i = 0; i < nts; ++i)
-      {
-	fread(&simTrackStates_[i], data_file.f_header.f_sizeof_trackstate, 1, fp);      
-      }
-    }
-    else
-    {
-      fseek(fp, nts * data_file.f_header.f_sizeof_trackstate, SEEK_CUR);
-      nts = -nts;
-    }
+    simTrackStates_.resize(nts);
+    fread(&simTrackStates_[0], sizeof(TrackState), nts, fp);
   }
 
   int nl;
