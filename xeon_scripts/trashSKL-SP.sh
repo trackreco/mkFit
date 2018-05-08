6b87fc213c5645260b8b37719f833428e1996c90
Dan Riley <Daniel.Riley@cornell.edu>
2018-05-08 12:34:55 -0700

    cleanup snb log

--- xeon_scripts/trashSKL-SP.sh
+++ xeon_scripts/trashSKL-SP.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Final cleanup script for benchmarks
-rm -rf benchmark_knl_dump.txt
+rm -rf benchmark_knl_dump.txt benchmark_snb_dump.txt
 rm -rf log_*.txt
 rm -rf *.root
 rm -rf *.png
