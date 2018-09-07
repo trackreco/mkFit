20db96b8333e954b335424c81ab9f00c9934183b
Slava Krutelyov <slava77@cern.ch>
2018-09-06 23:03:24 -0700

    undo inner bin selection, drop the pragma nounroll

--- mkFit/MkFinder.cc
+++ mkFit/MkFinder.cc
@@ -362,8 +362,8 @@ void MkFinder::SelectHitIndices(const LayerOfHits &layer_of_hits,
         // and issue prefetches at the same time.
         // Then enter vectorized loop to actually collect the hits in proper order.
 
-        //SK: ~20x1024 bin sizes give mostly 1 hit per bin
-#pragma nounroll
+        //SK: ~20x1024 bin sizes give mostly 1 hit per bin. Commented out for 128 bins or less
+        // #pragma nounroll
         for (uint16_t hi = L.m_phi_bin_infos[qi][pb].first; hi < L.m_phi_bin_infos[qi][pb].second; ++hi)
         {
           // MT: Access into m_hit_zs and m_hit_phis is 1% run-time each.
@@ -371,31 +371,24 @@ void MkFinder::SelectHitIndices(const LayerOfHits &layer_of_hits,
 	  if (Config::usePhiQArrays)
 	  {
             if (XHitSize[itrack] >= MPlexHitIdxMax) continue;
-            //inner bins should pass by construction
-            if (qi != qb1  && qb2 - 1 != qi  && pi != pb1 && pb2 - 1 != pi)
+
+            const float ddq   =       std::abs(q   - L.m_hit_qs[hi]);
+            const float ddphi = cdist(std::abs(phi - L.m_hit_phis[hi]));
+            
+            dprintf("     SHI %3d %4d %4d %5d  %6.3f %6.3f %6.4f %7.5f   %s\n",
+                    qi, pi, pb, hi,
+                    L.m_hit_qs[hi], L.m_hit_phis[hi], ddq, ddphi,
+                    (ddq < dq && ddphi < dphi) ? "PASS" : "FAIL");
+            
+            // MT: Removing extra check gives full efficiency ...
+            //     and means our error estimations are wrong!
+            // Avi says we should have *minimal* search windows per layer.
+            // Also ... if bins are sufficiently small, we do not need the extra
+            // checks, see above.
+            if (ddq < dq && ddphi < dphi)
             {
               XHitArr.At(itrack, XHitSize[itrack]++, 0) = hi;
             }
-            else //border bins
-            {
-              const float ddq   =       std::abs(q   - L.m_hit_qs[hi]);
-              const float ddphi = cdist(std::abs(phi - L.m_hit_phis[hi]));
-              
-              dprintf("     SHI %3d %4d %4d %5d  %6.3f %6.3f %6.4f %7.5f   %s\n",
-                      qi, pi, pb, hi,
-                      L.m_hit_qs[hi], L.m_hit_phis[hi], ddq, ddphi,
-                      (ddq < dq && ddphi < dphi) ? "PASS" : "FAIL");
-              
-              // MT: Removing extra check gives full efficiency ...
-              //     and means our error estimations are wrong!
-              // Avi says we should have *minimal* search windows per layer.
-              // Also ... if bins are sufficiently small, we do not need the extra
-              // checks, see above.
-              if (ddq < dq && ddphi < dphi)
-              {
-                XHitArr.At(itrack, XHitSize[itrack]++, 0) = hi;
-              }
-            }//inner or border bins
 	  }
 	  else
 	  {
@@ -406,10 +399,10 @@ void MkFinder::SelectHitIndices(const LayerOfHits &layer_of_hits,
 	      XHitArr.At(itrack, XHitSize[itrack]++, 0) = hi;
 	    }
 	  }
-        }
-      }
-    }
-  }
+        }//for (uint16_t hi =
+      }//pi
+    }//qi
+  }//itrack
 }
 
 
