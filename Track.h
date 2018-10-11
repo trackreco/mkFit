a99add878870feea3c96011f4bc4fbef56f15483
Mario Masciovecchio <mario.masciovecchio@cern.ch>
2018-10-11 11:39:30 -0700

    Extra cleaning of duplication for sortByScoreCandPair

--- Track.h
+++ Track.h
@@ -503,7 +503,6 @@ inline bool sortByScoreCand(const Track& cand1, const Track& cand2)
 
 inline bool sortByScoreStruct(const IdxChi2List& cand1, const IdxChi2List& cand2)
 {
-
   int nfoundhits[2] = {cand1.nhits,cand2.nhits};
   int nmisshits[2] = {cand1.nholes,cand2.nholes};
   float chi2[2] = {cand1.chi2,cand2.chi2};
@@ -513,11 +512,12 @@ inline bool sortByScoreStruct(const IdxChi2List& cand1, const IdxChi2List& cand2
 
 inline bool sortByScoreCandPair(const std::pair<Track, TrackState>& cand1, const std::pair<Track, TrackState>& cand2)
 {
-  int nfoundhits[2] = {cand1.first.nFoundHits(),cand2.first.nFoundHits()};
-  int nmisshits[2] = {cand1.first.nTotalHits()-cand1.first.nFoundHits(),cand2.first.nTotalHits()-cand2.first.nFoundHits()};
-  float chi2[2] = {cand1.first.chi2(),cand2.first.chi2()};
-  float pt[2] = {cand1.first.pT(),cand2.first.pT()};
-  return sortByScoreLoop(nfoundhits,nmisshits,chi2,pt);
+  return sortByScoreCand(cand1.first,cand2.first);
+//  int nfoundhits[2] = {cand1.first.nFoundHits(),cand2.first.nFoundHits()};
+//  int nmisshits[2] = {cand1.first.nTotalHits()-cand1.first.nFoundHits(),cand2.first.nTotalHits()-cand2.first.nFoundHits()};
+//  float chi2[2] = {cand1.first.chi2(),cand2.first.chi2()};
+//  float pt[2] = {cand1.first.pT(),cand2.first.pT()};
+//  return sortByScoreLoop(nfoundhits,nmisshits,chi2,pt);
 }
 
 
