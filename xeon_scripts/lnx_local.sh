dca0c5e57f024ebd19719c4db32f0ff87815b2d3
Tres Reid <tres.reid@gmail.com>
2019-05-01 11:22:18 -0700

    made standalone scripts to run with lnx

---
+++ xeon_scripts/lnx_local.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./xeon_scripts/benchmark-cmssw-ttbar-fulldet-build.sh LNX forPR
+./val_scripts/validation-cmssw-benchmarks.sh forPR
+./plotting/benchmarkPlots_lnx.sh forPR
+make distclean
