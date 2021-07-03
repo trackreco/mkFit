356a48aaec2dd2c2935b94655e70341d0458c56c
Mario Masciovecchio <mario.masciovecchio@cern.ch>
2021-07-03 11:31:19 -0700

    Update validation plotting to be consistent both with and without multi-iter

--- plotting/PlotValidation.cpp
+++ plotting/PlotValidation.cpp
@@ -29,13 +29,13 @@
 // h at the start == a different string version
 
 PlotValidation::PlotValidation(const TString & inName, const TString & outName, const Bool_t cmsswComp,  const int algo,
-			       const Bool_t mvInput, const Bool_t saveAs, const TString & outType)
+			       const Bool_t mvInput, const Bool_t rmSuffix, const Bool_t saveAs, const TString & outType)
   : fInName(inName), fOutName(outName), fCmsswComp(cmsswComp), fAlgo(algo),
-    fMvInput(mvInput), fSaveAs(saveAs), fOutType(outType)
+    fMvInput(mvInput), fRmSuffix(rmSuffix), fSaveAs(saveAs), fOutType(outType)
 {
   // Setup 
   PlotValidation::SetupStyle();
-  if (fAlgo>0) fOutName=fOutName+"_iter"+algo;
+  if (fAlgo>0 && !fRmSuffix) fOutName=fOutName+"_iter"+algo;
   PlotValidation::MakeOutDir(fOutName);
   PlotValidation::SetupBins();
   PlotValidation::SetupCommonVars();
@@ -51,7 +51,6 @@ PlotValidation::PlotValidation(const TString & inName, const TString & outName, 
   efftree = (TTree*)fInRoot->Get((fCmsswComp?"cmsswefftree":"efftree"));
   frtree  = (TTree*)fInRoot->Get((fCmsswComp?"cmsswfrtree":"frtree"));
   if(algo>0) frtree=frtree->CopyTree(Form("algorithm==%i",algo));
-
   // make output root file
   fOutRoot = new TFile(fOutName+"/plots.root", "RECREATE");
 }
@@ -276,15 +275,14 @@ void PlotValidation::PlotEffTree(int algo)
         {
 	  const auto refmask_trk  = refmask_trks [j];
 	  const auto duplmask_trk = duplmask_trks[j];
-    const auto itermask_trk = itermask_trks[j];
-    const auto iterduplmask_trk = iterduplmask_trks[j];
+	  const auto itermask_trk = itermask_trks[j];
+	  const auto iterduplmask_trk = iterduplmask_trks[j];
+	  
+	  const auto effIteration = algo>0?((itermask_trk>>algo)&1):1;
+	  const auto oneIteration = algo>0?((iterduplmask_trk>>algo)&1):1;
+	  const auto ineffIteration = algo>0?(((itermask_trk>>algo)&1) == 0):(refmask_trk == 0);
+	  const auto seedalgo_flag = (algoseed_trk>0 && algo>0)?((algoseed_trk>>algo)&1):1;
 
-    const auto effIteration = algo>0?((itermask_trk>>algo)&1):1;
-    const auto oneIteration = algo>0?((iterduplmask_trk>>algo)&1):1;
-    const auto ineffIteration = algo>0?(((itermask_trk>>algo)&1) == 0):(refmask_trk == 0);
-    const auto seedalgo_flag = (algoseed_trk>0 && algo>0)?((algoseed_trk>>algo)&1):1;
-    //if (algoseed_trk>0) std::cout << algoseed_trk << " "<<algo <<" "<< seedalgo_flag<<"  "<<effIteration<< std::endl;
-    //effIteration=effIteration&&seedalgo_flag;
 	  // plot key base
 	  const TString basekey = Form("%i_%i_%i",i,j,k);
 
