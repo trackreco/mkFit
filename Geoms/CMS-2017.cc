17899f5335d5332b485ffe741a8ed6e5bb98df47
Matevz Tadel <mtadel@ucsd.edu>
2021-08-04 23:30:25 -0700

    TopTec iteration, barrel - Move back fwd-search pickup by two layers as there are a lot of seeds with hits only in TOB1 and TOB2.

--- Geoms/CMS-2017.cc
+++ Geoms/CMS-2017.cc
@@ -111,7 +111,7 @@ namespace
     auto &spv = ic.m_steering_params;
     spv[TrackerInfo::Reg_Endcap_Neg]    .set_iterator_limits(12, 12, 24);
     spv[TrackerInfo::Reg_Transition_Neg].set_iterator_limits(27, 19, 39);
-    spv[TrackerInfo::Reg_Barrel]        .set_iterator_limits(14, 10, 14);
+    spv[TrackerInfo::Reg_Barrel]        .set_iterator_limits(12, 10, 14);
     spv[TrackerInfo::Reg_Transition_Pos].set_iterator_limits(27, 19, 39);
     spv[TrackerInfo::Reg_Endcap_Pos]    .set_iterator_limits(12, 12, 24);
   }
